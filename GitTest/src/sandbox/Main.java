d2668b16a5d8a02e7c76d25da7e3fe7fe1ef5f13
Daniel Andersen <dan@steria.dk>
2012-11-27 14:09:52 +0100

    test changes

--- GitTest/src/sandbox/Main.java
+++ GitTest/src/sandbox/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculator calc = new Calculator();
-		System.out.println(calc.sum(3, 4));
+		System.out.println(calc.sum(10, 4));
 		
 		
 
